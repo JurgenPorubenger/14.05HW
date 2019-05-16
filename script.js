89fc9464f46daf8b11be14c203eecad9c2b61b59
Eugene <jurgenporubenger@gmail.com>
2019-05-16 23:36:13 +0300

    16.05

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function func() {
     let variable= document.getElementById("numb").value; 
-    if (variable<=5&&variable!=Number) {
-        console.log("This is number :" + document.getElementById("numb").value);
+    if (variable<=5&&!isNaN(variable)) {
+        console.log("This is number: " + document.getElementById("numb").value);
     } 
-    else if (variable=String){
-        console.log("This is string :" + document.getElementById("numb").value);
-    } else {console.log("Shit!");}
+    else if (isNaN(variable)&&variable.length<=5){
+        console.log("This is string: " + document.getElementById("numb").value);
+    } else {console.log( variable.split(" "));}
 }
     document.getElementById("check").onclick = function() {
             func();   
