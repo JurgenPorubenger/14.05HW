b0bb8b039a9b0f8656f4fed938ab5da04228250c
Eugene <jurgen.porubenger@gmail.com>
2019-05-17 14:00:43 +0300

    17.05

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function func() {
     let variable= document.getElementById("numb").value; 
     if (variable<=5&&!isNaN(variable)) {
-        console.log("This is number: " + document.getElementById("numb").value);
+        console.log("This is number: " + variable);
     } 
     else if (isNaN(variable)&&variable.length<=5){
-        console.log("This is string: " + document.getElementById("numb").value);
+        if (/\s/.test(variable)) {
+            console.log("String contains whitespace: " + variable);
+        } else {
+            console.log("String not contains whitespace");
+        }
+        // console.log("This is string: " + variable);
     } else {console.log( variable.split(" "));}
 }
     document.getElementById("check").onclick = function() {
